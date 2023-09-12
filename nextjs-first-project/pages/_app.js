12f2c9f43d27bcccdb5d5c3a0979dfd9e00aa2ae
jingjielim <jingjie3000@gmail.com>
2023-09-11 22:53:05 -0400

    Remove extraneous code, Set up main page

--- nextjs-first-project/pages/_app.js
+++ nextjs-first-project/pages/_app.js
@@ -1,17 +1,5 @@
-import Head from 'next/head';
+import '@/styles/globals.css'
 
-import '../styles/globals.css';
-import Layout from '../components/layout/layout';
-
-function MyApp({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Head>
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-      </Head>
-      <Component {...pageProps} />
-    </Layout>
-  );
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
-
-export default MyApp;
---
+++ nextjs-first-project/pages/events/[id].js
@@ -0,0 +1,9 @@
+function EventDetailPage() {
+  return (
+    <div>
+      <h1>Single Event Detail</h1>
+    </div>
+  );
+}
+
+export default EventDetailPage;

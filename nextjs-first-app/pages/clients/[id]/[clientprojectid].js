b8edf0e9529a8c9d732b958a5487d89467bbaa92
jingjielim <jingjie3000@gmail.com>
2023-09-11 21:57:33 -0400

    Console logging router query of different routes

--- nextjs-first-app/pages/clients/[id]/[clientprojectid].js
+++ nextjs-first-app/pages/clients/[id]/[clientprojectid].js
@@ -1,7 +1,12 @@
+import { useRouter } from "next/router";
+
 function SelectedClientProjectPage() {
+  const router = useRouter()
+
+  console.log(router.query);
   return (
     <div>
-      <h1>The Project Page for a Specific Project for a Selected Client</h1>
+      <h1>The Project Page for a Specific Project for a Selected Client {router.query.id}</h1>
     </div>
   );
 }

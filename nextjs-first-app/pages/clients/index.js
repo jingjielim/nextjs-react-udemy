7eec983cfe82a992710952c8bfcec18dd90d7856
jingjielim <jingjie3000@gmail.com>
2023-09-11 22:14:03 -0400

    Navigating to Dynamic routes

--- nextjs-first-app/pages/clients/index.js
+++ nextjs-first-app/pages/clients/index.js
@@ -1,7 +1,20 @@
+import Link from "next/link";
+
 function ClientsPage() {
+  const clients = [
+    { id: "jj", name: "Jing Jie" },
+    { id: "max", name: "Max" },
+  ];
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={clients.id}>
+            <Link href={`clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }

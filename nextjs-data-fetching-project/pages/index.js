423464f7cc2eaa95fda5840601d170e00e34b3bc
jingjielim <jingjie3000@gmail.com>
2023-09-16 11:22:51 -0400

    Incremental Static Generation: A look behind the scenes

--- nextjs-data-fetching-project/pages/index.js
+++ nextjs-data-fetching-project/pages/index.js
@@ -13,11 +13,17 @@ function HomePage(props) {
 }
 
 export async function getStaticProps() {
+  console.log('(Re-)Generating...')
   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
-  return { props: { products: data.products } };
+  return {
+    props: {
+      products: data.products,
+    },
+    revalidate: 10,
+  };
 }
 
 export default HomePage;

d8fd1012125b8fd76950cf502445f4d5a666046b
Jing Jie Lim <jingjiel@amazon.com>
2023-09-29 11:50:40 -0400

    Configuring the "head" content

--- nextjs-prj-data-fetching/pages/index.js
+++ nextjs-prj-data-fetching/pages/index.js
@@ -1,10 +1,15 @@
 import { getFeaturedEvents } from "../helpers/api-util";
 import EventList from "../components/events/event-list";
+import Head from "next/head";
 
 function HomePage(props) {
   return (
     <div>
       <EventList items={props.events} />
+      <Head>
+        <title>NextJS Events</title>
+        <meta name="description" content="Find the best events" />
+      </Head>
     </div>
   );
 }
